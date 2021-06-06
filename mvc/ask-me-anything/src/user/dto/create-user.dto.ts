b78fe4bd530c8debf45f9b1156593043355f76fb
Christos Serafeidis <ch.serafi@gmail.com>
2021-06-06 18:55:01 +0300

    Added global class validation

--- mvc/ask-me-anything/src/user/dto/create-user.dto.ts
+++ mvc/ask-me-anything/src/user/dto/create-user.dto.ts
@@ -1,7 +1,24 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateUserDto {
+
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsOptional()
+  @IsString()
   firstName: string;
+
+  @IsOptional()
+  @IsString()
   lastName: string;
 }
--- mvc/ask-me-anything/src/user/dto/update-user.dto.ts
+++ mvc/ask-me-anything/src/user/dto/update-user.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+
+  @IsString()
+  @IsOptional()
+  readonly firstName: string;
+
+  @IsString()
+  @IsOptional()
+  readonly lastName: string;
+}
