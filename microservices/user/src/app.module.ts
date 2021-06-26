9c669ddee6e196e327b8d8fbc4a0a8054b420eea
Christos Serafeidis <ch.serafi@gmail.com>
2021-06-26 14:46:12 +0300

    Changed user microservice app module

--- microservices/user/src/app.module.ts
+++ microservices/user/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user.module';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [UserModule, TypeOrmModule.forRoot()],
