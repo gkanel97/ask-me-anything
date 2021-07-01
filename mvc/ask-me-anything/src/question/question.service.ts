4eba1b991081e95a1deb5cb4466952dac87578c6
Christos Serafeidis <ch.serafi@gmail.com>
2021-07-01 14:29:30 +0300

    Changed statistics queries to use ORM

--- mvc/ask-me-anything/src/question/question.service.ts
+++ mvc/ask-me-anything/src/question/question.service.ts
@@ -5,6 +5,7 @@ import { User } from "../user/entities/user.entity";
 import { InjectEntityManager } from "@nestjs/typeorm";
 import { EntityManager, getManager } from "typeorm";
 import { Question } from "./entities/question.entity";
+import { first } from "rxjs/operators";
 
 @Injectable()
 export class QuestionService {
@@ -71,17 +72,35 @@ export class QuestionService {
     });
   }
 
-  async getQuestionsPerDay(n) {
-    return this.manager.query("SELECT DATE(updateDate) AS date, COUNT(id) AS count FROM questions GROUP BY date ORDER BY date DESC LIMIT ($1);", [n]);
-    // return this.manager
-    //   .createQueryBuilder()
-    //   .select("DATE(question.updateDate)", "date")
-    //   .addSelect("COUNT(question.id)", "count")
-    //   .from(Question, "question")
-    //   .groupBy("date");
+  async getQuestionsPerDay(n: number) {
+    // return this.manager.query("SELECT DATE(updateDate) AS date, COUNT(id) AS count FROM questions GROUP BY date ORDER BY date DESC LIMIT ($1);", [n]);
+    const firstDay = new Date();
+    firstDay.setDate(firstDay.getDate() - n);
+
+    return this.manager
+      .createQueryBuilder(Question, "q")
+      .select("DATE(updateDate)", "date")
+      .addSelect("COUNT(id)", "count")
+      .where("date > DATE(:dayInterval)", {dayInterval: firstDay.toISOString()})
+      .groupBy("date")
+      .orderBy("date", "DESC")
+      .getRawMany();
   }
 
   async getMyQuestionsPerDay(n: number, uuid: string) {
-    return this.manager.query("SELECT DATE(updateDate) AS date, COUNT(id) AS count FROM questions WHERE userID = $1 GROUP BY date ORDER BY date DESC LIMIT $2;", [uuid ,n]);
+    // return this.manager.query("SELECT DATE(updateDate) AS date, COUNT(id) AS count FROM questions WHERE userID = $1 GROUP BY date ORDER BY date DESC LIMIT $2;", [uuid ,n]);
+
+    const firstDay = new Date();
+    firstDay.setDate(firstDay.getDate() - n);
+
+    return this.manager
+      .createQueryBuilder(Question, "q")
+      .select("DATE(updateDate)", "date")
+      .addSelect("COUNT(id)", "count")
+      .where("userID = :userId", {userId: uuid})
+      .andWhere("date > DATE(:dayInterval)", {dayInterval: firstDay.toISOString()})
+      .groupBy("date")
+      .orderBy("date", "ASC")
+      .getRawMany();
   }
 }
