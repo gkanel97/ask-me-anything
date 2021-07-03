f7a7420fec7a56070dc5f502c7f20116cff2c60b
Christos Serafeidis <ch.serafi@gmail.com>
2021-07-03 19:20:24 +0300

    Implemented contribution charts using ChartJS

--- frontend/ask-me-anything/src/components/home.jsx
+++ frontend/ask-me-anything/src/components/home.jsx
@@ -1,7 +1,119 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Chart, registerables } from 'chart.js';
+import { httpGet } from '../scripts/requests';
+import { KEYWORD_BASE_URL, QUESTION_BASE_URL } from '../configuration/URLS';
 
 class Home extends Component {
+
+    async componentDidMount() {
+        Chart.register(...registerables);
+
+        async function questionsChart() {
+            async function formatData() {
+                let formattedData = await httpGet(`${QUESTION_BASE_URL}/getQuestionsPerDay/7`)
+                .then(payload => {
+                    return  payload.reduce( (acc, v) => { return {...acc, [v.date]: v.count} }, {});
+                });
+                let currentDay = new Date();
+                let today = new Date();
+                let data = new Array();
+                currentDay.setDate(currentDay.getDate() - 7);
+                while (currentDay <= today) {
+                    let dateString = currentDay.toISOString().split('T')[0];
+                    let dateCount = formattedData[dateString] ?? 0;
+                    data.push({date: dateString, count: dateCount});
+                    currentDay.setDate(currentDay.getDate() + 1);
+                }
+                return data;
+            }
+
+            const ctx = document.getElementById('questionFrequencyChart').getContext('2d');
+            const cfg = {
+                type: 'line',
+                data: {
+                    datasets: [{
+                        label: 'Questions per day',
+                        data: await formatData(),
+                        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        parsing: {
+                            xAxisKey: 'date',
+                            yAxisKey: 'count'
+                        }
+                    }]
+                },
+                options: {
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Questions per day',
+                            color: 'rgba(255, 255, 255, 1)',
+                            font: {
+                                size: 22,
+                                weight: 'normal'
+                            }
+                        },
+                        legend: {
+                            display: false
+                        }
+                    }
+                }
+            };
+
+            new Chart(ctx, cfg);
+        }
+
+        async function keywordsChart() {
+            const ctx = document.getElementById('keywordFrequencyChart').getContext('2d');
+            const cfg = {
+                type: 'bar',
+                data: {
+                    datasets: [{
+                        label: 'Questions per day',
+                        data: await httpGet(`${KEYWORD_BASE_URL}/questionsPerKeyword`),
+                        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        parsing: {
+                            yAxisKey: 'keyword',
+                            xAxisKey: 'count'
+                        }
+                    }]
+                },
+                options: {
+                    indexAxis: 'y',
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Most popular keywords',
+                            color: 'rgba(255, 255, 255, 1)',
+                            font: {
+                                size: 22,
+                                weight: 'normal'
+                            }
+                        },
+                        legend: {
+                            display: false
+                        }
+                    },
+                    scales: {
+                        xAxis: {
+                            ticks: {
+                                precision: 0,
+                                stepSize: 1
+                            }
+                        }
+                    }
+                }
+            };
+
+            new Chart(ctx, cfg);
+        }
+
+        await questionsChart();
+        await keywordsChart();
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -9,17 +121,12 @@ class Home extends Component {
                 <div className="row" style={{ minHeight: "50vh" }}>
                     <div className="col-lg-5 my-2">
                         <div className="h-100 p-5 text-white bg-dark rounded-3">
-                            <h4>Questions per day chart</h4>
+                            <canvas id="questionFrequencyChart" width="95%" height="95%"></canvas>
                         </div>
                     </div>
                     <div className="col-lg-5 my-2">
                         <div className="h-100 p-5 text-white bg-dark rounded-3">
-                            <h5>Top 10 Keywords</h5>
-                            <ol>
-                                <li>Lorem - 100</li>
-                                <li>Ipsum - 88</li>
-                                <li>Dolor - 42</li>
-                            </ol>
+                            <canvas id="keywordFrequencyChart" width="95%" height="95%"></canvas>
                         </div>
                     </div>
                     <div className="col-lg-2 my-2 p-0">
--- frontend/ask-me-anything/src/components/myHome.jsx
+++ frontend/ask-me-anything/src/components/myHome.jsx
@@ -1,16 +1,94 @@
 import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
-import QuestionLg from '../icons/questionLg';
+import { Chart, registerables } from 'chart.js';
+import { httpGet, httpGetProtected } from '../scripts/requests';
+import { ANSWER_BASE_URL, QUESTION_BASE_URL } from '../configuration/URLS';
 
 class MyHome extends Component {
+    async componentDidMount() {
+        Chart.register(...registerables);
+
+        async function formatData(url) {
+            let formattedData = await httpGetProtected(url)
+            .then(payload => {
+                return  payload.reduce( (acc, v) => { return {...acc, [v.date]: v.count} }, {});
+            });
+            let currentDay = new Date();
+            let today = new Date();
+            let data = new Array();
+            currentDay.setDate(currentDay.getDate() - 7);
+            while (currentDay <= today) {
+                let dateString = currentDay.toISOString().split('T')[0];
+                let dateCount = formattedData[dateString] ?? 0;
+                data.push({date: dateString, count: dateCount});
+                currentDay.setDate(currentDay.getDate() + 1);
+            }
+            return data;
+        }
+
+        const ctx = document.getElementById('contributionsChart').getContext('2d');
+        const cfg = {
+            type: 'bar',
+            data: {
+                datasets: [{
+                    label: 'Questions per day',
+                    data: await formatData(`${QUESTION_BASE_URL}/getMyQuestionsPerDay/7`),
+                    backgroundColor: 'rgba(54, 162, 235, 0.9)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    parsing: {
+                        xAxisKey: 'date',
+                        yAxisKey: 'count'
+                    }
+                },
+                {
+                    label: 'Answers per day',
+                    data: await formatData(`${ANSWER_BASE_URL}/getMyAnswersPerDay/7`),
+                    backgroundColor: 'rgba(255, 159, 64, 0.9)',
+                    borderColor: 'rgba(255, 159, 64, 1)',
+                    parsing: {
+                        xAxisKey: 'date',
+                        yAxisKey: 'count'
+                    }
+                }]
+            },
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'My contributions per day',
+                        color: 'rgba(255, 255, 255, 1)',
+                        font: {
+                            size: 22,
+                            weight: 'normal'
+                        }
+                    }
+                },
+                scales: {
+                    x: {
+                        stacked: true
+                    },
+                    y: {
+                        stacked: true,
+                        ticks: {
+                            precision: 0,
+                            stepSize: 1
+                        }
+                    }
+                }
+            }
+        };
+
+        new Chart(ctx, cfg);
+    }
+
     render() {
         return (
             <React.Fragment>
                 <h2>Welcome to My.AskMeAnything</h2>
-                <div className="row" style={{ minHeight: "50vh" }}>
-                    <div className="col-lg-9 my-2">
+                <div className="row" style={{ minHeight: "50vh", maxHeight: "80vh" }}>
+                    <div className="col-lg-9 my-2" >
                         <div className="h-100 p-5 text-white bg-dark rounded-3">
-                            <h4>Questions per day chart</h4>
+                            <canvas id="contributionsChart"></canvas>
                         </div>
                     </div>
                     <div className="col-lg-3 my-2 p-0">
